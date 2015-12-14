741c5713fe464741bdd688206e988e454c5d3e28
dpg3k <dpg3k@d-137-155-231.bootp.Virginia.EDU>
2015-12-14 16:18:46 -0500

    Call the user service and report the outcome

--- jobs/userservice.rb
+++ jobs/userservice.rb
@@ -1,7 +1,16 @@
-random = Random.new
+require 'httparty'
 
 SCHEDULER.every '10s' do
 
-  status = random.rand(0..1) == 1
-  send_event('ldap', { text: status ? 'OK' : 'Down' })
+  response = HTTParty.get('http://docker1.lib.virginia.edu:8010/healthcheck')
+  begin
+     if response['ldap']['healthy'] == true
+        send_event('ldap', { text: 'OK' })
+     else
+        send_event('ldap', { text: 'Down' })
+     end
+  rescue => e
+    send_event('ldap', { text: 'Down' })
+  end
+
 end
