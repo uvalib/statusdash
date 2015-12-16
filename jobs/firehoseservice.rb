3e9067f3e6559fcaa2e0494fb95b4fd07814b769
dpg3k <dpg3k@d-137-155-231.bootp.Virginia.EDU>
2015-12-16 13:26:28 -0500

    Add meters for page load time

--- jobs/firehoseservice.rb
+++ jobs/firehoseservice.rb
@@ -3,16 +3,16 @@ require 'httparty'
 SCHEDULER.every '30s', allow_overlapping: false do
 
   service_url = ENV[ 'FIREHOSE_SERVICE_URL' ]
-  data_sink_ldap = 'oracle-firehose'
+  data_sink = 'oracle-firehose'
   begin
      response = HTTParty.get( service_url )
      if response.code == 200 && response['ldap'] && response['ldap']['healthy'] == 'true'
-        send_event( data_sink_ldap, { text: 'Up' })
+        send_event( data_sink, { text: 'Up' })
      else
-        send_event( data_sink_ldap, { text: 'Down' })
+        send_event( data_sink, { text: 'Down' })
      end
   rescue => e
-    send_event( data_sink_ldap, { text: 'Down' })
+    send_event( data_sink, { text: 'Down' })
   end
 
 end
--- jobs/virgo.rb
+++ jobs/virgo.rb
@@ -3,16 +3,27 @@ require 'httparty'
 SCHEDULER.every '30s', allow_overlapping: false do
 
   service_url = ENV[ 'VIRGO_URL' ]
-  data_sink_ldap = 'virgo'
+  data_sink_availability = 'virgo'
+  data_sink_response = 'virgo-time'
+  max_response_time = 1500
+
   begin
+     start_time = Time.now
      response = HTTParty.get( service_url )
+     end_time = Time.now
      if response.code == 200
-        send_event( data_sink_ldap, { text: 'Up' })
+        response_time = ( ( end_time - start_time ).to_f * 1000 ).to_i
+        response_time = max_response_time unless response_time < max_response_time
+
+        send_event( data_sink_availability, { text: 'Up' })
+        send_event( data_sink_response, { value: response_time } )
      else
-        send_event( data_sink_ldap, { text: 'Down' })
+        send_event( data_sink_availability, { text: 'Down' })
+        send_event( data_sink_response, { value: 0 } )
      end
   rescue => e
-    send_event( data_sink_ldap, { text: 'Down' })
+    send_event( data_sink_availability, { text: 'Down' })
+    send_event( data_sink_response, { value: 0 } )
   end
 
 end
