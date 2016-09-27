c4cf003634fb9654ce7870aeb218f1fc83c639a8
UVADave <dpg3k@virginia.edu>
2016-09-27 14:50:30 -0400

    LIBRA-563: make redis endpoint heartbeat more reliable

--- jobs/redis_service.rb
+++ jobs/redis_service.rb
@@ -1,3 +1,5 @@
+require 'redis'
+
 SCHEDULER.every '60s', allow_overlapping: false do
 
   config = CONFIG['REDIS']
@@ -5,10 +7,10 @@ SCHEDULER.every '60s', allow_overlapping: false do
   data_sink = config['id']
 
   begin
-     tokens = service_url.split( ':' )
-     s = TCPSocket.open( tokens[ 0 ], tokens[ 1 ] )
-     s.close
-     send_event( data_sink, { text: 'Up' })
+    redis = Redis.new( { url: service_url } )
+    redis.ping
+    redis.close
+    send_event( data_sink, { text: 'Up' })
   rescue => e
     send_event( data_sink, { text: 'Down' })
     LOGGER.error "#{config['title']} returns error: #{e.to_s}"
