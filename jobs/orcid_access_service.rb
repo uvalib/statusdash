cdcd1924091a2c26846140e78da9fd20d137dd83
UVADave <dpg3k@virginia.edu>
2016-11-17 11:15:35 -0500

    Add ORCID access service to dashboard

---
+++ jobs/orcid_access_service.rb
@@ -0,0 +1,21 @@
+SCHEDULER.every '60s', allow_overlapping: false do
+
+  config = CONFIG['ORCID_ACCESS_SERVICE']
+
+  service_url = config['url']
+  data_sink = config['id']
+
+  begin
+    response = Requester.get( service_url )
+    if response.code == 200
+      send_event( data_sink, { text: 'Up' })
+    else
+      send_event( data_sink, { text: 'Down' })
+      LOGGER.error "#{config['title']} returns status: #{response.code}"
+    end
+  rescue => e
+    send_event( data_sink, { text: 'Down' })
+    LOGGER.error "#{config['title']} returns error: #{e.to_s}"
+  end
+
+end
